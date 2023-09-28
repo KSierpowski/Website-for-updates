d1a460e38abc4c9d8e542c451bb6852a1ddb6aea
KSierpowski <sierpowskikacper@gmail.com>
2023-09-28 18:36:48 +0200

    Added slide-out header menu

--- Wedesign_en/js/scripts.js
+++ Wedesign_en/js/scripts.js
@@ -1,7 +1,15 @@
 
 // Mobile Menu
 
+document.getElementById("hamburguer-icon").onclick = function(){
 
+document.getElementById("sliding-header-menu-outer").style.right = "0";
+
+}
+
+document.getElementById("sliding-header-menu-close-button").onclick = function(){
+  document.getElementById("sliding-header-menu-outer").style.right = "-320px";
+}
 
 // About us Tab
 
